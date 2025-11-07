836f2e007724316f3d50d389f1fa85a1fdc9795b
Jim Chen <theyee0@gmail.com>
2025-11-07 15:49:35 -0500

    Implemented basic neural network with 2 convolutional layers and one fully connected layer

---
+++ model.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+# https://github.com/BarakOshri/ConvChess/blob/master/papers/ConvChess.pdf
+
+class StackedConvolve(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+
+        self.layer_stack = nn.Sequential(
+            nn.Conv2d(12, 1, 3, padding=1),
+            nn.Linear(8 * 8, 16 * 16),
+            nn.ReLU(),
+            nn.Conv2d(1, 1, 3, padding=1),
+            nn.Linear(16*16, 8*8),
+            nn.ReLU(),
+            nn.Linear(8*8, 8*8),
+            nn.ReLU()
+        )
+
+        return
+
+    def forward(self, x):
+        return self.layer_stack(x)
