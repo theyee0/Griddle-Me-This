9a45b9eb7fc247079298477d4f78f86d53eb9333
Jim Chen <theyee0@gmail.com>
2025-11-09 19:27:24 -0500

    Fixed bugs with tensor/list usage and added missing items to piece list

--- utils.py
+++ utils.py
@@ -5,28 +5,28 @@ import chess
 def chess_to_tensor(board):
     """Return vector as 12 bitmaps of the board showing where black and white pieces are"""
 
-    types = (chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.QUEEN, chess.KING)
+    types = (chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING)
     colors = (chess.WHITE, chess.BLACK)
 
-    pieces = map(chess.board.piece_at, chess.SQUARES)
+    pieces = tuple(map(board.piece_at, chess.SQUARES))
 
     # Function that returns 1 if parameters match
     piece_selector = lambda x: lambda y: 1 if x == y else 0
     # Turns a game board into ones and zeroes depending if a piece is found
-    piece_tensor = lambda x, y: torch.Tensor(map(piece_selector(chess.Piece(x, y)), pieces))
+    piece_tensor = lambda x, y: torch.Tensor(list(map(piece_selector(chess.Piece(x, y)), pieces))).reshape(8, 8)
 
     # Returns a list of all 8x8 tensors for both black and white pieces
-    subtensors = [torch.chunk(piece_tensor(x, y), 8) for x in types for y in colors]
+    subtensors = [piece_tensor(x, y) for x in types for y in colors]
 
     # Returns the list of tensors as a tensor itself
-    return torch.Tensor(subtensors)
+    return torch.stack(subtensors)
 
 
 def tensor_to_chess(tensor):
     """Given a one-hot tensor, return a chess.board object"""
 
     board = chess.Board()
-    types = (chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.QUEEN, chess.KING)
+    types = (chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING)
     colors = (chess.WHITE, chess.BLACK)
 
     # Generate (TYPE, COLOR) pairs
@@ -51,7 +51,7 @@ def tensor_to_chess(tensor):
 def position_to_tensor(position):
     """Returns a one-hot encoded flat tensor representation of a position"""
 
-    pieces = map(lambda x: position == x, chess.SQUARES)
+    pieces = list(map(lambda x: position == x, chess.SQUARES))
 
     return torch.Tensor(pieces)
 
